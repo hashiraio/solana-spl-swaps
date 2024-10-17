9d664297b2e001e92aaa2ba9f5b3720bde996ce1
Shiva Kiran K <shiva_kr@riseup.net>
2024-10-17 15:44:32 +0530

    feat/lib.rs: Add swap inititation

---
+++ programs/solana-spl-swaps/src/lib.rs
@@ -0,0 +1,108 @@
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::hash;
+use anchor_spl::{
+    token,
+    token::{Mint, Token, TokenAccount},
+};
+declare_id!("FeEiy23gsDpr6W7sxzcxEDJVXayYRNhkfBrkwLg2oYgn");
+
+type Lamports = u64;
+type Slots = u32;
+
+#[program]
+pub mod solana_spl_swaps {
+    use super::*;
+
+    pub fn initiate(
+        ctx: Context<Initiate>,
+        secret_hash: [u8; 32],
+        redeemer: Pubkey,
+        amount: Lamports,
+        expires_in: Slots,
+    ) -> Result<()> {
+        let Initiate {
+            initiator,
+            initiator_wallet,
+            swap_wallet,
+            token_program,
+            ..
+        } = ctx.accounts;
+        let swap_id = hash::hash(
+            &[initiator.key().as_ref(), &secret_hash].concat()
+        ).to_bytes();
+        *ctx.accounts.swap_account = SwapAccount {
+            swap_id,
+            initiator: initiator.key(),
+            redeemer,
+            secret_hash,
+            expiry_slot: Clock::get()?.slot + expires_in as u64,
+            amount,
+        };
+        let cpi_context = CpiContext::new(
+            token_program.to_account_info(),
+            token::Transfer {
+                from: initiator_wallet.to_account_info(),
+                to: swap_wallet.to_account_info(),
+                authority: initiator.to_account_info(),
+            }
+        );
+        token::transfer(cpi_context, amount)?;
+        emit!(Initiated { swap_id, secret_hash, amount });
+        
+        Ok(())
+    }
+}
+
+#[account]
+pub struct SwapAccount {
+    swap_id: [u8; 32],
+    initiator: Pubkey,
+    redeemer: Pubkey,
+    secret_hash: [u8; 32],
+    expiry_slot: u64,
+    amount: Lamports,
+}
+
+#[derive(Accounts)]
+#[instruction(secret_hash: [u8; 32])]
+pub struct Initiate<'info> {
+    #[account(
+        init,
+        payer = initiator,
+        seeds = [b"swap_account".as_ref(), initiator.key().as_ref(), secret_hash.as_ref()],
+        bump,
+        space = 8 + std::mem::size_of::<SwapAccount>(),
+    )]
+    pub swap_account: Account<'info, SwapAccount>,
+
+    #[account(
+        init,
+        payer = initiator,
+        seeds = [b"swap_wallet".as_ref(), initiator.key().as_ref(), secret_hash.as_ref()],
+        bump,
+        token::mint = mint,
+        token::authority = swap_account,
+    )]
+    pub swap_wallet: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub initiator: Signer<'info>,
+
+    #[account(
+        mut,
+        token::mint = mint,
+        token::authority = initiator,
+    )]
+    pub initiator_wallet: Account<'info, TokenAccount>,
+
+    pub mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+
+#[event]
+pub struct Initiated {
+    swap_id: [u8; 32],
+    secret_hash: [u8; 32],
+    amount: u64,
+}
---
+++ tests/solana-spl-swaps.ts
@@ -0,0 +1,87 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program, web3 } from "@coral-xyz/anchor";
+import * as spl from "@solana/spl-token";
+
+import { expect } from "chai";
+import * as crypto from 'crypto';
+
+import { SolanaSplSwaps } from "../target/types/solana_spl_swaps";
+
+// Configure the client to use the local cluster.
+anchor.setProvider(anchor.AnchorProvider.env());
+const connection = anchor.getProvider().connection;
+const program = anchor.workspace.SolanaSplSwaps as Program<SolanaSplSwaps>;
+const MILLIS_PER_SLOT = 400;
+
+describe("Testing one way swap between Alice and Bob", () => {
+    const swapAmount = new anchor.BN(10);
+    const swapExpiresIn = 1000 / MILLIS_PER_SLOT; // 1 second
+    const secret: Uint8Array = crypto.randomBytes(32);
+    const secretHash = [...crypto.createHash('sha256').update(secret).digest()];
+
+    let mint: web3.PublicKey;  // A random mint for the test
+    const mintAuthority = new web3.Keypair();  // And a random authority for it
+    const mintAuthorityPubkey = mintAuthority.publicKey;  // And his pubkey
+
+    // Alice is the initiator here
+    // Alice's stuff
+    const alice = new web3.Keypair();     // Alice's Solana Keypair
+    const alicePubkey = alice.publicKey;  // Alice's Solan address
+    let aliceWallet: web3.PublicKey;      // Alice's Token Wallet
+
+    // Bob's stuff
+    const bob = new web3.Keypair();       // Bob's Solana Keypair
+    const bobPubkey = bob.publicKey;      // Bob's Solana Address
+    let bobWallet: web3.PublicKey;        // Bob's Token Wallet
+
+    // Deriving PDAs, just for logging and test verification
+    const pdaSeeds = (phrase: string) => [Buffer.from(phrase), alicePubkey.toBuffer(), Buffer.from(secretHash)];
+    const [swapAccount,] = web3.PublicKey.findProgramAddressSync(pdaSeeds("swap_account"), program.programId);
+    const [swapWallet,] = web3.PublicKey.findProgramAddressSync(pdaSeeds("swap_wallet"), program.programId);
+
+    before(async () => {
+        // Fund alice's wallet with 1 SOL, her funds will be used for setting up the test as well
+        await connection.requestAirdrop(alicePubkey, 1_000_000_000)
+            .then(async signature =>
+                await connection.confirmTransaction({ signature, ...(await connection.getLatestBlockhash()) })
+            );
+
+        // Create Mint and Associated Token Accounts
+        mint = await spl.createMint(connection, alice, mintAuthorityPubkey, null, 0);
+        aliceWallet = await spl.createAssociatedTokenAccount(connection, alice, mint, alicePubkey);
+        bobWallet = await spl.createAssociatedTokenAccount(connection, alice, mint, bobPubkey);
+
+        // Fund alice's token wallet with 100 tokens
+        await spl.mintTo(connection, alice, mint, aliceWallet, mintAuthority, 100)
+            .then(async signature =>
+                await connection.confirmTransaction({ signature, ...(await connection.getLatestBlockhash()) })
+            );
+        console.log(
+            `Account Information:
+    Alice: \t${alicePubkey}\t Alice TokenWallet: \t${aliceWallet}
+    Bob: \t${bobPubkey}\t Bob TokenWallet: \t${bobWallet}
+    Swap Acc: \t${swapAccount}\t Swap Wallet: \t\t${swapWallet}
+    Mint: \t${mint}\t Mint Authority: \t${mintAuthorityPubkey}\n`
+        );
+    });
+
+    async function aliceInitiate() {
+        await program.methods.initiate(secretHash, bobPubkey, swapAmount, swapExpiresIn)
+            .accounts({
+                initiator: alicePubkey,
+                initiatorWallet: aliceWallet,
+                mint,
+            })
+            .signers([alice]).rpc()
+            .then(async signature => {
+                console.log("Alice initiated with Signature:", signature);
+                await connection.confirmTransaction({signature, ...(await connection.getLatestBlockhash())});
+            });
+    }
+
+    it("Test initiation", async () => {
+        await aliceInitiate();
+        const swapWalletBalance = (await connection.getTokenAccountBalance(swapWallet)).value.amount;
+        expect(swapWalletBalance).to.equal(swapAmount.toString());
+    });
+});
