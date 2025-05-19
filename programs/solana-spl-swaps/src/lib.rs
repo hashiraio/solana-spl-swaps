678bd41d4a20978ca9d8995cc6ef9837d08b5c01
Shiva Kiran K <shiva_kr@riseup.net>
2025-05-19 12:34:02 +0530

    fix: emit all swap parameters for off-chain verification

--- programs/solana-spl-swaps/src/lib.rs
+++ programs/solana-spl-swaps/src/lib.rs
@@ -4,10 +4,7 @@ use anchor_spl::{
     token,
     token::{Mint, Token, TokenAccount},
 };
-declare_id!("FeEiy23gsDpr6W7sxzcxEDJVXayYRNhkfBrkwLg2oYgn");
-
-type Lamports = u64;
-type Slots = u32;
+declare_id!("2WXpY8havGjfRxme9LUxtjFHTh1EfU3ur4v6wiK4KdNC");
 
 #[program]
 pub mod solana_spl_swaps {
@@ -15,41 +12,44 @@ pub mod solana_spl_swaps {
 
     pub fn initiate(
         ctx: Context<Initiate>,
-        swap_id: [u8; 32],
-        redeemer_wallet: Pubkey,
+        swap_amount: u64, // In base units of the token
+        expires_in_slots: u64,
+        redeemer_token_account: Pubkey,
         secret_hash: [u8; 32],
-        amount: Lamports,
-        expires_in: Slots,
     ) -> Result<()> {
         let Initiate {
             initiator,
-            initiator_wallet,
-            swap_wallet,
+            initiator_token_account,
+            swap_token_account,
             token_program,
             ..
         } = ctx.accounts;
+
         *ctx.accounts.swap_account = SwapAccount {
-            swap_id,
+            expiry_slot: Clock::get()?.slot + expires_in_slots,
             initiator: initiator.key(),
-            redeemer_wallet,
+            redeemer_token_account,
             secret_hash,
-            expiry_slot: Clock::get()?.slot + expires_in as u64,
-            amount,
+            swap_amount,
             bump: ctx.bumps.swap_account,
         };
+
         let cpi_context = CpiContext::new(
             token_program.to_account_info(),
             token::Transfer {
-                from: initiator_wallet.to_account_info(),
-                to: swap_wallet.to_account_info(),
+                from: initiator_token_account.to_account_info(),
+                to: swap_token_account.to_account_info(),
                 authority: initiator.to_account_info(),
             },
         );
-        token::transfer(cpi_context, amount)?;
+        token::transfer(cpi_context, swap_amount)?;
+
         emit!(Initiated {
-            swap_id,
+            initiator: initiator.key(),
+            expires_in_slots,
+            redeemer_token_account,
             secret_hash,
-            amount
+            swap_amount,
         });
 
         Ok(())
@@ -57,45 +57,54 @@ pub mod solana_spl_swaps {
 
     pub fn redeem(ctx: Context<Redeem>, secret: [u8; 32]) -> Result<()> {
         let Redeem {
-            swap_wallet,
+            swap_token_account,
             swap_account,
             initiator,
-            redeemer_wallet,
+            redeemer_token_account,
             token_program,
             ..
         } = ctx.accounts;
         let SwapAccount {
-            swap_id,
-            secret_hash,
             bump,
-            amount,
+            secret_hash,
+            swap_amount,
             ..
         } = **swap_account;
 
         require!(
-            hash::hash(&secret).as_ref() == &secret_hash,
+            hash::hash(&secret).to_bytes() == secret_hash,
             SwapError::InvalidSecret
         );
 
-        let pda_seeds: &[&[&[u8]]] = &[&[b"swap_account", &swap_id, &[bump]]];
+        let initiator_key = initiator.key();
+        let pda_seeds: &[&[&[u8]]] = &[&[
+            b"swap_account",
+            initiator_key.as_ref(),
+            &secret_hash,
+            &[bump],
+        ]];
         // Transfer the tokens to the redeemer
         let cpi_context = CpiContext::new(
             token_program.to_account_info(),
             token::Transfer {
-                from: swap_wallet.to_account_info(),
-                to: redeemer_wallet.to_account_info(),
+                from: swap_token_account.to_account_info(),
+                to: redeemer_token_account.to_account_info(),
                 authority: swap_account.to_account_info(),
             },
         )
         .with_signer(pda_seeds);
-        token::transfer(cpi_context, amount)?;
+        token::transfer(cpi_context, swap_amount)?;
 
-        emit!(Redeemed { swap_id, secret });
+        emit!(Redeemed {
+            initiator: initiator.key(),
+            secret
+        });
+
         // Close the swap wallet (returns rent lamports to initiator)
         let cpi_context = CpiContext::new(
             token_program.to_account_info(),
             token::CloseAccount {
-                account: swap_wallet.to_account_info(),
+                account: swap_token_account.to_account_info(),
                 destination: initiator.to_account_info(),
                 authority: swap_account.to_account_info(),
             },
@@ -110,42 +119,51 @@ pub mod solana_spl_swaps {
         let Refund {
             token_program,
             swap_account,
-            swap_wallet,
+            swap_token_account,
             initiator,
-            initiator_wallet,
+            initiator_token_account,
             ..
         } = ctx.accounts;
         let SwapAccount {
-            expiry_slot,
-            swap_id,
-            amount,
             bump,
+            expiry_slot,
+            secret_hash,
+            swap_amount,
             ..
         } = **swap_account;
 
         require!(
-            Clock::get()?.slot >= expiry_slot,
+            Clock::get()?.slot > expiry_slot,
             SwapError::RefundBeforeExpiry
         );
 
-        let pda_seeds: &[&[&[u8]]] = &[&[b"swap_account", &swap_id, &[bump]]];
+        let initiator_key = initiator.key();
+        let pda_seeds: &[&[&[u8]]] = &[&[
+            b"swap_account",
+            initiator_key.as_ref(),
+            &secret_hash,
+            &[bump],
+        ]];
         let cpi_context = CpiContext::new(
             token_program.to_account_info(),
             token::Transfer {
-                from: swap_wallet.to_account_info(),
-                to: initiator_wallet.to_account_info(),
+                from: swap_token_account.to_account_info(),
+                to: initiator_token_account.to_account_info(),
                 authority: swap_account.to_account_info(),
             },
         )
         .with_signer(pda_seeds);
-        token::transfer(cpi_context, amount)?;
+        token::transfer(cpi_context, swap_amount)?;
 
-        emit!(Refunded { swap_id });
+        emit!(Refunded {
+            initiator: initiator_key,
+            secret_hash,
+        });
 
         let cpi_context = CpiContext::new(
             token_program.to_account_info(),
             token::CloseAccount {
-                account: swap_wallet.to_account_info(),
+                account: swap_token_account.to_account_info(),
                 destination: initiator.to_account_info(),
                 authority: swap_account.to_account_info(),
             },
@@ -160,38 +178,47 @@ pub mod solana_spl_swaps {
         let InstantRefund {
             token_program,
             swap_account,
-            swap_wallet,
+            swap_token_account,
             initiator,
-            initiator_wallet,
+            initiator_token_account,
             ..
         } = ctx.accounts;
         let SwapAccount {
-            swap_id,
-            amount,
             bump,
+            secret_hash,
+            swap_amount,
             ..
         } = **swap_account;
 
-        let pda_seeds: &[&[&[u8]]] = &[&[b"swap_account", &swap_id, &[bump]]];
+        let initiator_key = initiator.key();
+        let pda_seeds: &[&[&[u8]]] = &[&[
+            b"swap_account",
+            initiator_key.as_ref(),
+            &secret_hash,
+            &[bump],
+        ]];
 
         let cpi_context = CpiContext::new(
             token_program.to_account_info(),
             token::Transfer {
-                from: swap_wallet.to_account_info(),
-                to: initiator_wallet.to_account_info(),
+                from: swap_token_account.to_account_info(),
+                to: initiator_token_account.to_account_info(),
                 authority: swap_account.to_account_info(),
             },
         )
         .with_signer(pda_seeds);
-        token::transfer(cpi_context, amount)?;
+        token::transfer(cpi_context, swap_amount)?;
 
-        emit!(InstantRefunded { swap_id });
+        emit!(InstantRefunded {
+            initiator: initiator_key,
+            secret_hash
+        });
 
         // Close the wallet, refunding rent lamports to initiator
         let cpi_context = CpiContext::new(
             token_program.to_account_info(),
             token::CloseAccount {
-                account: swap_wallet.to_account_info(),
+                account: swap_token_account.to_account_info(),
                 destination: initiator.to_account_info(),
                 authority: swap_account.to_account_info(),
             },
@@ -204,38 +231,40 @@ pub mod solana_spl_swaps {
 }
 
 #[account]
+#[derive(InitSpace)]
 pub struct SwapAccount {
-    swap_id: [u8; 32],
-    initiator: Pubkey,
-    redeemer_wallet: Pubkey, // Redeemer's token wallet
-    secret_hash: [u8; 32],
-    expiry_slot: u64,
-    amount: Lamports,
-    bump: u8, // Used for pda_seeds in refund & redeem
+    pub bump: u8, // Needed for PDA signatures
+    pub expiry_slot: u64,
+    pub initiator: Pubkey,
+    pub redeemer_token_account: Pubkey,
+    pub secret_hash: [u8; 32],
+    pub swap_amount: u64, // In base units of the token
 }
 
 #[derive(Accounts)]
-#[instruction(swap_id: [u8; 32])]
+// Refer: https://www.anchor-lang.com/docs/references/account-constraints#instruction-attribute
+#[instruction(swap_amount: u64, expires_in_slots: u64, redeemer_token_account: Pubkey, secret_hash: [u8; 32])]
 pub struct Initiate<'info> {
     #[account(
         init,
         payer = initiator,
-        seeds = [b"swap_account".as_ref(), &swap_id],
+        seeds = [b"swap_account", initiator.key().as_ref(), &secret_hash],
         bump,
-        space = 8 + std::mem::size_of::<SwapAccount>(),
+        space = 8 + SwapAccount::INIT_SPACE,
     )]
     pub swap_account: Account<'info, SwapAccount>,
 
     #[account(
         init,
         payer = initiator,
-        seeds = [b"swap_wallet".as_ref(), &swap_id],
+        seeds = [b"swap_token_account", initiator.key().as_ref(), &secret_hash],
         bump,
         token::mint = mint,
         token::authority = swap_account,
     )]
-    pub swap_wallet: Account<'info, TokenAccount>,
+    pub swap_token_account: Account<'info, TokenAccount>,
 
+    // The initiator must sign this transaction
     #[account(mut)]
     pub initiator: Signer<'info>,
 
@@ -244,7 +273,7 @@ pub struct Initiate<'info> {
         token::mint = mint,
         token::authority = initiator,
     )]
-    pub initiator_wallet: Account<'info, TokenAccount>,
+    pub initiator_token_account: Account<'info, TokenAccount>,
 
     pub mint: Account<'info, Mint>,
     pub token_program: Program<'info, Token>,
@@ -257,13 +286,13 @@ pub struct Redeem<'info> {
     pub swap_account: Account<'info, SwapAccount>,
 
     #[account(mut, token::authority = swap_account)]
-    pub swap_wallet: Account<'info, TokenAccount>,
+    pub swap_token_account: Account<'info, TokenAccount>,
 
-    #[account(mut, address = swap_account.redeemer_wallet @ SwapError::InvalidRedeemer)]
-    pub redeemer_wallet: Account<'info, TokenAccount>,
+    #[account(mut, address = swap_account.redeemer_token_account @ SwapError::InvalidRedeemer)]
+    pub redeemer_token_account: Account<'info, TokenAccount>,
 
     /// CHECK: Initiator's address for refunding PDA rent amounts
-    #[account(mut, address = swap_account.initiator @ SwapError::InvalidRefundee)]
+    #[account(mut, address = swap_account.initiator @ SwapError::InvalidInitiator)]
     pub initiator: AccountInfo<'info>,
 
     pub token_program: Program<'info, Token>,
@@ -275,14 +304,14 @@ pub struct Refund<'info> {
     pub swap_account: Account<'info, SwapAccount>,
 
     #[account(mut, token::authority = swap_account)]
-    pub swap_wallet: Account<'info, TokenAccount>,
+    pub swap_token_account: Account<'info, TokenAccount>,
 
     /// CHECK: Initiator's address for refunding PDA rent amounts
-    #[account(mut, address = swap_account.initiator @ SwapError::InvalidRefundee)]
+    #[account(mut, address = swap_account.initiator @ SwapError::InvalidInitiator)]
     pub initiator: AccountInfo<'info>,
 
     #[account(mut)]
-    pub initiator_wallet: Account<'info, TokenAccount>,
+    pub initiator_token_account: Account<'info, TokenAccount>,
 
     pub token_program: Program<'info, Token>,
 }
@@ -293,58 +322,61 @@ pub struct InstantRefund<'info> {
     pub swap_account: Account<'info, SwapAccount>,
 
     #[account(mut, token::authority = swap_account)]
-    pub swap_wallet: Account<'info, TokenAccount>,
+    pub swap_token_account: Account<'info, TokenAccount>,
 
     /// CHECK: Initiator's address for PDA rent refund
     #[account(mut)]
     pub initiator: AccountInfo<'info>,
 
     #[account(mut, token::authority = initiator)]
-    pub initiator_wallet: Account<'info, TokenAccount>,
+    pub initiator_token_account: Account<'info, TokenAccount>,
 
     /// CHECK: Redeemer's address for signature verification
     #[account(mut)]
     pub redeemer: Signer<'info>,
 
+    // `redeemer` must be the authority for the swap's `redeemer_token_account`
     #[account(
         mut,
         token::authority = redeemer,
-        address = swap_account.redeemer_wallet @ SwapError::InvalidRedeemer,
+        address = swap_account.redeemer_token_account @ SwapError::InvalidRedeemer,
     )]
-    pub redeemer_wallet: Account<'info, TokenAccount>,
+    pub redeemer_token_account: Account<'info, TokenAccount>,
 
     pub token_program: Program<'info, Token>,
 }
 
 #[event]
 pub struct Initiated {
-    swap_id: [u8; 32],
-    secret_hash: [u8; 32],
-    amount: u64,
+    pub swap_amount: u64,
+    pub expires_in_slots: u64,
+    pub initiator: Pubkey,
+    pub redeemer_token_account: Pubkey,
+    pub secret_hash: [u8; 32],
 }
 #[event]
 pub struct Redeemed {
-    swap_id: [u8; 32],
-    secret: [u8; 32],
+    pub initiator: Pubkey,
+    pub secret: [u8; 32],
 }
 #[event]
 pub struct Refunded {
-    swap_id: [u8; 32],
+    pub initiator: Pubkey,
+    pub secret_hash: [u8; 32],
 }
 #[event]
 pub struct InstantRefunded {
-    swap_id: [u8; 32],
+    pub initiator: Pubkey,
+    pub secret_hash: [u8; 32],
 }
 
 #[error_code]
 pub enum SwapError {
-    #[msg("The provided redeemer is not the intended recipient of the swap amount")]
-    InvalidRedeemer,
+    #[msg("The provided initiator is not the original initiator of this swap account")]
+    InvalidInitiator,
 
-    #[msg(
-        "The provided initiator/refundee is not the original initiator of the given swap account"
-    )]
-    InvalidRefundee,
+    #[msg("The provided redeemer is not the original redeemer of this swap amount")]
+    InvalidRedeemer,
 
     #[msg("The provided secret does not correspond to the secret hash in the swap account")]
     InvalidSecret,
--- tests/solana-spl-swaps.ts
+++ tests/solana-spl-swaps.ts
@@ -1,7 +1,6 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program, web3 } from "@coral-xyz/anchor";
 import * as spl from "@solana/spl-token";
-
 import { expect } from "chai";
 import * as crypto from 'crypto';
 
@@ -14,133 +13,128 @@ const program = anchor.workspace.SolanaSplSwaps as Program<SolanaSplSwaps>;
 
 describe("Testing one way swap between Alice and Bob", () => {
     const swapAmount = new anchor.BN(10);
-    const expiryMs = 1000;
-    // Expiry must be provided in Slot units where 1 slot = 400 ms
-    const swapExpiresIn = expiryMs / 400;
+    const swapExpiresIn = new anchor.BN(2); // 2 slots = 800 ms
     const secret: Buffer = crypto.randomBytes(32);
     const secretHash: Buffer = crypto.createHash('sha256').update(secret).digest();
 
     let mint: web3.PublicKey;  // A random mint for the test
     const mintAuthority = new web3.Keypair();  // And a random authority for it
-    const mintAuthorityPubkey = mintAuthority.publicKey;  // And his pubkey
 
     // Alice is the initiator here
     // Alice's stuff
-    const alice = new web3.Keypair();     // Alice's Solana Keypair
-    const alicePubkey = alice.publicKey;  // Alice's Solan address
-    let aliceWallet: web3.PublicKey;      // Alice's Token Wallet
+    const alice = new web3.Keypair();
+    let aliceTokenAccount: web3.PublicKey;
 
-    const swapIdPreImage = Buffer.concat([alicePubkey.toBuffer(), secretHash]);
-    const swapId = crypto.createHash('sha256').update(swapIdPreImage).digest();
     // Bob's stuff
-    const bob = new web3.Keypair();       // Bob's Solana Keypair
-    const bobPubkey = bob.publicKey;      // Bob's Solana Address
-    let bobWallet: web3.PublicKey;        // Bob's Token Wallet
+    const bob = new web3.Keypair();
+    let bobTokenAccount: web3.PublicKey;
 
-    // Deriving PDAs, just for logging and test verification
-    const pdaSeeds = (phrase: string) => [Buffer.from(phrase), swapId];
+    const pdaSeeds = (phrase: string) => [Buffer.from(phrase), alice.publicKey.toBuffer(), secretHash];
     const [swapAccount,] = web3.PublicKey.findProgramAddressSync(pdaSeeds("swap_account"), program.programId);
-    const [swapWallet,] = web3.PublicKey.findProgramAddressSync(pdaSeeds("swap_wallet"), program.programId);
+    const [swapTokenAccount,] = web3.PublicKey.findProgramAddressSync(pdaSeeds("swap_token_account"), program.programId);
+
+    let latestBlockHash: web3.BlockhashWithExpiryBlockHeight;
 
     before(async () => {
+        latestBlockHash = await connection.getLatestBlockhash();
         // Fund alice's wallet with 1 SOL, her funds will be used for setting up the test as well
-        await connection.requestAirdrop(alicePubkey, 1_000_000_000)
-            .then(async signature =>
-                await connection.confirmTransaction({ signature, ...(await connection.getLatestBlockhash()) })
-            );
+        const signature = await connection.requestAirdrop(alice.publicKey, 1_000_000_000);
+        await connection.confirmTransaction({ signature, ...latestBlockHash });
 
         // Create Mint and Associated Token Accounts
-        mint = await spl.createMint(connection, alice, mintAuthorityPubkey, null, 0);
-        aliceWallet = await spl.createAssociatedTokenAccount(connection, alice, mint, alicePubkey);
-        bobWallet = await spl.createAssociatedTokenAccount(connection, alice, mint, bobPubkey);
+        mint = await spl.createMint(connection, alice, mintAuthority.publicKey, null, 0);
+        aliceTokenAccount = await spl.createAssociatedTokenAccount(connection, alice, mint, alice.publicKey);
+        bobTokenAccount = await spl.createAssociatedTokenAccount(connection, alice, mint, bob.publicKey);
 
         // Fund alice's token wallet with 100 tokens
-        await spl.mintTo(connection, alice, mint, aliceWallet, mintAuthority, 100)
-            .then(async signature =>
-                await connection.confirmTransaction({ signature, ...(await connection.getLatestBlockhash()) })
-            );
+        await spl.mintTo(connection, alice, mint, aliceTokenAccount, mintAuthority, 100)
+        await connection.confirmTransaction({ signature, ...latestBlockHash })
+
         console.log(
             `Account Information:
-\tAlice:    ${alicePubkey} \t Alice TokenWallet: \t${aliceWallet}
-\tBob:      ${bobPubkey} \t Bob TokenWallet: \t${bobWallet}
-\tSwap Acc: ${swapAccount} \t Swap Wallet: \t\t${swapWallet}
-\tMint:     ${mint}\t Mint Authority: \t${mintAuthorityPubkey}\n`
+\tAlice:    ${alice.publicKey} \t Alice TokenWallet: \t${aliceTokenAccount}
+\tBob:      ${bob.publicKey} \t Bob TokenWallet: \t${bobTokenAccount}
+\tSwap Acc: ${swapAccount} \t Swap Wallet: \t\t${swapTokenAccount}
+\tMint:     ${mint}\t Mint Authority: \t${mintAuthority.publicKey}\n`
         );
     });
 
     async function aliceInitiate() {
-        await program.methods
-            .initiate([...swapId], bobWallet, [...secretHash], swapAmount, swapExpiresIn)
+        const signature = await program.methods
+            .initiate(swapAmount, swapExpiresIn, bobTokenAccount, [...secretHash])
             .accounts({
-                initiator: alicePubkey,
-                initiatorWallet: aliceWallet,
+                initiator: alice.publicKey,
+                initiatorTokenAccount: aliceTokenAccount,
                 mint,
             })
-            .signers([alice]).rpc()
-            .then(async signature => {
-                console.log(`\tInitiate: \t${signature}`);
-                await connection.confirmTransaction({signature, ...(await connection.getLatestBlockhash())});
-            });
+            .signers([alice])
+            .rpc();
+        await connection.confirmTransaction({signature, ...latestBlockHash});
+        console.log(`\tInitiate: \t${signature}`);
     }
 
     it("Test initiation", async () => {
         await aliceInitiate();
-        const swapWalletBalance = (await connection.getTokenAccountBalance(swapWallet)).value.amount;
+        const swapWalletBalance = (await connection.getTokenAccountBalance(swapTokenAccount)).value.amount;
         expect(swapWalletBalance).to.equal(swapAmount.toString());
     });
 
     it("Test redeem", async () => {
         // The previous testcase has initiated the swap
-        await program.methods.redeem([...secret])
-        .accounts({
-            swapAccount,
-            swapWallet,
-            redeemerWallet: bobWallet,
-            initiator: alicePubkey,
-        }).rpc().then(async signature => {
-            console.log(`\tRedeem: \t${signature}`);
-            await connection.confirmTransaction({signature, ...(await connection.getLatestBlockhash())});
-        });
-        const bobBalance = (await connection.getTokenAccountBalance(bobWallet)).value.amount;
+        const signature = await program.methods.redeem([...secret])
+            .accounts({
+                initiator: alice.publicKey,
+                redeemerTokenAccount: bobTokenAccount,
+                swapAccount,
+                swapTokenAccount,
+            })
+            .rpc();
+        await connection.confirmTransaction({signature, ...latestBlockHash});
+        console.log(`\tRedeem: \t${signature}`);
+
+        const bobBalance = (await connection.getTokenAccountBalance(bobTokenAccount)).value.amount;
         expect(bobBalance).to.equal(swapAmount.toString());
     });
 
     it("Test refund", async () => {
         await aliceInitiate();  // Re-initiating for this test
-        console.log(`Awaiting timelock of ${expiryMs} for Refund`);
+        const expiryMs = swapExpiresIn.toNumber() * 400;
+        console.log(`Awaiting timelock of ${expiryMs}ms for Refund`);
         await new Promise(r => setTimeout(r, expiryMs + 400));
-        await program.methods.refund()
-        .accounts({
-            swapAccount,
-            swapWallet,
-            initiator: alicePubkey,
-            initiatorWallet: aliceWallet,
-        }).rpc().then(async signature => {
-            console.log(`\tRefund: \t${signature}`);
-            await connection.confirmTransaction({ signature, ...(await connection.getLatestBlockhash()) });
-        });
+        const signature = await program.methods.refund()
+            .accounts({
+                swapAccount,
+                swapTokenAccount: swapTokenAccount,
+                initiator: alice.publicKey,
+                initiatorTokenAccount: aliceTokenAccount,
+            })
+            .rpc();
+        await connection.confirmTransaction({ signature, ...latestBlockHash });
+        console.log(`\tRefund: \t${signature}`);
+
         // Alice had a token balance of 80 (-10 from above init(), -10 from previous swap)
-        const aliceBalance = (await connection.getTokenAccountBalance(aliceWallet)).value.amount;
+        const aliceBalance = (await connection.getTokenAccountBalance(aliceTokenAccount)).value.amount;
         expect(aliceBalance).to.equal('90');  // Successful redeem adds +10 making it 90
     });
 
     it("Test instant refund", async () => {
         await aliceInitiate();  // Re-initiating for this test
-        await program.methods.instantRefund()
-        .accounts({
-            swapAccount,
-            swapWallet,
-            initiator: alicePubkey,
-            initiatorWallet: aliceWallet,
-            redeemer: bobPubkey,
-            redeemerWallet: bobWallet,
-        }).signers([bob])
-        .rpc().then(async signature => {
-            console.log(`\tInstant Refund:  ${signature}`);
-            await connection.confirmTransaction({ signature, ...(await connection.getLatestBlockhash()) });
-        });
+        const signature = await program.methods.instantRefund()
+            .accounts({
+                initiator: alice.publicKey,
+                initiatorTokenAccount: aliceTokenAccount,
+                redeemer: bob.publicKey,
+                redeemerTokenAccount: bobTokenAccount,
+                swapAccount,
+                swapTokenAccount: swapTokenAccount,
+            })
+            .signers([bob])
+            .rpc();
+        await connection.confirmTransaction({ signature, ...latestBlockHash });
+        console.log(`\tInstant Refund:  ${signature}`);
+
         // Alice had a token balance of 80 (-10 from above init(), -10 from previous swap)
-        const aliceBalance = (await connection.getTokenAccountBalance(aliceWallet)).value.amount;
+        const aliceBalance = (await connection.getTokenAccountBalance(aliceTokenAccount)).value.amount;
         expect(aliceBalance).to.equal('90');  // Successful redeem adds +10 making it 90
     });
 });
